17fdb82a43ac8acebe9f34306e4c04a94f8a3523
gerardo.leal <gerardo.leal@lubelsoft.com>
2019-10-03 17:45:11 -0500

    Se modifica la clase desde eclipse para probar el flujo: eclipse + github + jenkins

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello World!, hello Jenkins.");
+    System.out.println("Hello World!, hello Jenkins, hello Eclipse.");
   }
 }
