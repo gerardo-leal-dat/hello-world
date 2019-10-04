c9351f3e43f005fda69e9638e6ab1a540c2ce0d7
gerardo.leal <gerardo.leal@lubelsoft.com>
2019-10-03 19:42:37 -0500

    Primera modificación de sonar

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello World!, hello Jenkins, hello Eclipse.");
-  }
+
+	private HelloWorld() {}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!, hello Jenkins, hello Eclipse.");
+	}
 }
