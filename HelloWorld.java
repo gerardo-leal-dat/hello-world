cb2d556d3f0da349193094bfb7f94de2da7303df
gerardo.leal <gerardo.leal@lubelsoft.com>
2019-12-19 16:42:25 -0600

    new feautre

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Modificación de código...");
-		System.out.println("Modificación de código...");
+		System.out.println("Cambio de rama...");
 	}
 
 }
